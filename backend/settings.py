510afc92109b50d69f19b2af0f577c61e319fc0c
Manish Gupta <manish644508@gmail.com>
2026-08-18 12:17:49 +0530

    Fix Cors-headers and home view

--- backend/settings.py
+++ backend/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMidlleware"
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -137,6 +137,6 @@ MAILERS = {
     },
 }
 
-CORS_ALLOW_ALL_ORIGINS  = [
-  "http://127.0.0.1:8000/"
+CORS_ALLOWED_ORIGINS  = [
+  "http://127.0.0.1:8000",
 ]
--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 
 # Create your views here.
-def home():
+def home(request):
   data = {
     'message':'Welocome to the E-commerce store!'
   }
